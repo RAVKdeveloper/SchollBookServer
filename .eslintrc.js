e13544c28f33bb4ad6254ef56db72213636655f5
unknown <shcherbakovkirill2@gmail.com>
2024-04-06 19:23:49 +0300

    init: linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'plugin:import/recommended',
   ],
   root: true,
   env: {
@@ -17,9 +18,39 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': ['warn', { endOfLine: 'auto' }],
+    'no-console': ['warn'],
+    'no-var': ['error'],
+    quotes: ['error', 'single'],
+    'prefer-const': ['error'],
+    'max-len': ['error', { code: 100 }],
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling'],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '*.css',
+            group: 'index',
+            patternOptions: {
+              matchBase: true,
+            },
+            position: 'after',
+          },
+          {
+            pattern: '@/**',
+            group: 'external',
+            position: 'after',
+          },
+        ],
+      },
+    ],
+    'import/no-unresolved': 'off',
+    'react-hooks/exhaustive-deps': 'off',
   },
-};
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+import { ConfigService } from '@nestjs/config'
+import { ValidationPipe } from '@nestjs/common'
+import * as cookieParser from 'cookie-parser'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+
+  const configService = app.get(ConfigService)
+
+  app.enableCors({ credentials: true, origin: 'http://localhost:3000' })
+  app.setGlobalPrefix('api')
+  app.useGlobalPipes(new ValidationPipe())
+  app.use(cookieParser())
+
+  const config = new DocumentBuilder()
+    .setTitle('Sber bank')
+    .setDescription('API Sberbank')
+    .setVersion('1.0')
+    .addTag('sberbank')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+
+  SwaggerModule.setup('docs', app, document)
+
+  await app.listen(configService.get('port'))
 }
-bootstrap();
+bootstrap()
